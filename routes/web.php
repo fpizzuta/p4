cf1467dc8601bcdb2a694420fd91def94fcd8861
Frank Pizzuta <frank@pizzuta.com>
2018-11-21 14:28:15 -0500

    database working

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,31 @@ Route::get('/newgame2', 'GameController@create2');
 Route::get('/user/{name}', 'UserController@show');
 Route::get('/users', 'UserController@index');
 
+Route::get('/debug', function () {
+
+    $debug = [
+        'Environment' => App::environment(),
+    ];
+
+    /*
+    The following commented out line will print your MySQL credentials.
+    Uncomment this line only if you're facing difficulties connecting to the
+    database and you need to confirm your credentials. When you're done
+    debugging, comment it back out so you don't accidentally leave it
+    running on your production server, making your credentials public.
+    */
+    #$debug['MySQL connection config'] = config('database.connections.mysql');
+
+    try {
+        $databases = DB::select('SHOW DATABASES;');
+        $debug['Database connection test'] = 'PASSED';
+        $debug['Databases'] = array_column($databases, 'Database');
+    } catch (Exception $e) {
+        $debug['Database connection test'] = 'FAILED: '.$e->getMessage();
+    }
+
+    dump($debug);
+});
+
 
 
